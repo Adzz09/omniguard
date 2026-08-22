fafe2b96b4fd0dd2e145cd2b746dcfa8cb092e5a
peter14l <peter14l@users.noreply.github.com>
2026-08-22 08:17:32 +0530

    fix(wear): use backgroundScope in WearMainViewModelTest

--- app-wear/src/test/java/com/omniguard/wear/WearMainViewModelTest.kt
+++ app-wear/src/test/java/com/omniguard/wear/WearMainViewModelTest.kt
@@ -1,13 +1,8 @@
 package com.omniguard.wear
 
-import com.omniguard.wear.hardware.WearPhysicalButtonAction
-import com.omniguard.wear.hardware.WearPhysicalButtonReceiver
-import com.omniguard.wear.haptics.WearHapticController
 import com.omniguard.wear.presentation.WearMainViewModel
 import com.omniguard.wear.presentation.WearScreenRoute
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.StandardTestDispatcher
-import kotlinx.coroutines.test.TestScope
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
@@ -17,12 +12,9 @@ import org.junit.Test
 @OptIn(ExperimentalCoroutinesApi::class)
 class WearMainViewModelTest {
 
-    private val testDispatcher = StandardTestDispatcher()
-    private val testScope = TestScope(testDispatcher)
-
     @Test
-    fun testFallDetectionWorkflowOnWear() = testScope.runTest {
-        val viewModel = WearMainViewModel(viewModelScope = testScope)
+    fun testFallDetectionWorkflowOnWear() = runTest {
+        val viewModel = WearMainViewModel(viewModelScope = backgroundScope)
 
         viewModel.onFallDetected(impactG = 4.2f, countdownSeconds = 60)
         assertEquals(WearScreenRoute.FALL_COUNTDOWN, viewModel.uiState.value.currentRoute)
@@ -38,8 +30,8 @@ class WearMainViewModelTest {
     }
 
     @Test
-    fun testWearNavigationSyncUpdates() {
-        val viewModel = WearMainViewModel(viewModelScope = testScope)
+    fun testWearNavigationSyncUpdates() = runTest {
+        val viewModel = WearMainViewModel(viewModelScope = backgroundScope)
 
         viewModel.updateNavigation(
             icon = "↱",
@@ -55,8 +47,8 @@ class WearMainViewModelTest {
     }
 
     @Test
-    fun testWearGeofenceStatusUpdate() {
-        val viewModel = WearMainViewModel(viewModelScope = testScope)
+    fun testWearGeofenceStatusUpdate() = runTest {
+        val viewModel = WearMainViewModel(viewModelScope = backgroundScope)
 
         viewModel.updateGeofenceStatus("Tuition", "4:02 PM")
         assertEquals(WearScreenRoute.STATUS, viewModel.uiState.value.currentRoute)
